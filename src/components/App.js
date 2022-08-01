9538dfb3222d8a01919fc9b679d05ffd3cc7e6e9
rafaelbchs <rafaelbchacins@gmail.com>
2022-08-01 09:48:17 -0500

    reinstalled dependencies

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
+    return <h1>Hello Simu</h1>
+}
+
+
+export default App;
