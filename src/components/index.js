fd7696e8c059f5a34735c34b9a1a31d33b786181
rafaelbchs <rafaelbchacins@gmail.com>
2022-08-03 12:07:07 -0500

    finished adding activity

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Login } from "./Login";
 export { default as Register } from "./Register";
 export { default as Routines } from "./Routines"
 export { default as MyRoutines } from "./MyRoutines"
+export { default as EachRoutine } from "./EachRoutine"
